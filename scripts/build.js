1e007b41e3eb3bac1733b5e11bc80d14498a5977
Ryan Weiss <rw3iss@gmail.com>
2021-02-16 10:48:58 +0200

    Updated versions

---
+++ scripts/build.js
@@ -0,0 +1,25 @@
+// Config: relative to where npm command is run:
+const SRC_DIR = 'src';
+const OUTPUT_DIR = 'build';
+const IS_DEV = true;
+const TARGET = 'es2018';
+
+function build(entryFile, outFile) {
+    require('esbuild').build({
+        entryPoints: [entryFile],
+        outfile: outFile,
+        platform: 'node',
+        bundle: true,
+        define: { "process.env.NODE_ENV": IS_DEV ? "\"development\"" : "\"product\"" },
+        target: TARGET,
+        logLevel: 'silent'
+    })
+    .then(r => { console.log("Build succeeded.") })
+    .catch((e) => {
+        console.log("Error building:", e.message);
+        process.exit(1)
+    })
+}
+
+build(`${SRC_DIR}/index.ts`, `${OUTPUT_DIR}/index.js`);
+build(`${SRC_DIR}/scripts/generateMigrations.ts`, `${OUTPUT_DIR}/generateMigrations.js`);
